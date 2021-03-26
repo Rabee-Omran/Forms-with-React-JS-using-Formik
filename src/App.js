a6f1ff895226640562db8c86a6ea432efdd21ee8
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-26 22:52:35 +0300

    add more form fields

--- src/App.js
+++ src/App.js
@@ -9,10 +9,27 @@ class App extends Component{
 
   form = (props) =>{
     return <form onSubmit = {props.handleSubmit}>
+
     <label>Name : </label>
     <Field name = "name"/><hr/>
+
     <label>E-Mail : </label>
-    <Field name = "email"/><hr/>
+    <Field name = "email" type="email"/><hr/>
+
+    <label>Select : </label>
+    <Field name = "type" component="select">
+        <option value = "1"> One</option>  
+        <option value = "2"> Two</option>  
+    </Field><hr/>
+
+    <label>Checkbox : </label>
+    <Field name = "active" type="checkbox"/><hr/>
+
+
+    <label>Radio : </label>
+    <Field name = "category" type="radio" value = '1'/>
+    <Field name = "category" type="radio" value = '2'/><hr/>
+
       <button type = "submit"> Send</button>
     </form>
   }
@@ -25,7 +42,7 @@ class App extends Component{
       
       <div>
 
-          <Formik initialValues = {{name: "", email: ""}}onSubmit = {this.onSubmit} >
+          <Formik initialValues = {{name: "", email: "",type: "", active:false, category:'1'}}onSubmit = {this.onSubmit} >
             {this.form}
           </Formik>
 
