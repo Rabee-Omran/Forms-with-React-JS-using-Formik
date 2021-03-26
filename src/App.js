5931972cd15ccb52217834433fc3d9295f84e5aa
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-26 22:45:28 +0300

    install formik and build simple form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
-
+import {Formik, Field} from 'formik';
 class App extends Component{
 
-  state = {
-    value: 'Default',
-    selectVal:2,
-    checked:true,
-    radioValue : '2'
+
+  onSubmit = (values) => {
+      console.log(values)
   }
 
-  setValue = (event) => {
-    this.setState({
-      value: event.target.value
-    })
-  }
-  setSelectValue = (event) => {
-    this.setState({
-      selectVal: event.target.value
-    })
+  form = (props) =>{
+    return <form onSubmit = {props.handleSubmit}>
+    <label>Name : </label>
+    <Field name = "name"/><hr/>
+    <label>E-Mail : </label>
+    <Field name = "email"/><hr/>
+      <button type = "submit"> Send</button>
+    </form>
   }
 
-  setCheckedValue = (event) => {
-    this.setState({
-      checked: event.target.checked
-    })
-  }
-  changeRadio = (event) => {
-    this.setState({
-      radioValue: event.target.value
-    })
-  }
 
   render(){
 
@@ -38,21 +25,9 @@ class App extends Component{
       
       <div>
 
-
-          <input value={this.state.value} onChange={this.setValue}/> <hr/>    
-          <textarea value={this.state.value} onChange={this.setValue}/> <hr/>  
-          <select value= {this.state.selectVal} onChange={this.setSelectValue} >
-            <option value = '1'> One</option>
-            <option value = '2'> Two</option>
-            <option value = '3'> Three</option>
-          </select><hr/>  
-          <input type = "checkbox" checked = {this.state.checked} onChange={this.setCheckedValue}/>
-          <label>Checkbox</label><hr/>  
-          <div onChange={this.changeRadio}>    
-              <input type = "radio" value = '1' checked= {this.state.radioValue === '1' } /> One
-              <input type = "radio" value = '2' checked= {this.state.radioValue === '2' } /> Two
-          </div><hr/> 
-
+          <Formik initialValues = {{name: "", email: ""}}onSubmit = {this.onSubmit} >
+            {this.form}
+          </Formik>
 
       </div>
     );
